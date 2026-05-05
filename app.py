9aa7e84cc6ae2f5f708296bfa41134d80fa68869
Enimi25 <baskinltd@gmail.com>
2026-05-05 12:35:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-import os
 
 app = FastAPI()
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 @app.get("/")
 def home():
-    return FileResponse(os.path.join(BASE_DIR, "index.html"))
+    return FileResponse("index.html")
 
 @app.get("/widget.js")
 def widget():
-    return FileResponse(os.path.join(BASE_DIR, "widget.js"))
+    return FileResponse("widget.js")
